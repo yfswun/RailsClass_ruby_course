4a3102a0ab5f8342c7a96aad231d7f7105e25016
Sylvia Wun <yfswun@yahoo.com>
2013-12-17 01:06:43 -0800

    Commented out validation code

--- app/models/url.rb
+++ app/models/url.rb
@@ -7,8 +7,8 @@ class Url < ActiveRecord::Base
     # Write code the validates urls have a domain name of herokuapp.com.
 
     # domain = 'herokuapp.com'
-    validates :url, format: { with: /.*herokuapp.com\z/i,
-                              message: "The URL must be hosted by Heroku." }
+    # validates :url, format: { with: /.*herokuapp.com\z/i,
+                              # message: "The URL must be hosted by Heroku." }
     
     # validates :url, uniqueness: { case_sensitive: false
                                 # , message: "This URL has already been registered." }
