1ad7447d0bce08e7be7b4de32bf3a32aa999501c
Sylvia Wun <yfswun@yahoo.com>
2013-12-17 19:34:44 -0800

    production.rb: set config.assets.compile = true; deleted folder public/assets; precompiled for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,9 @@ CloudRegistry::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  # config.assets.compile = false
+  # http://stackoverflow.com/questions/17332822/heroku-production-differing-from-local-development-rails
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
